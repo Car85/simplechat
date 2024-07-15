0f289a52a153b4f8e62c20739847488873a7737f
Car85 <car85@car85.car85>
2024-07-15 02:21:58 -0700

    init SimpleChat

---
+++ lib/simplechat/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Simplechat.Mailer do
+  use Swoosh.Mailer, otp_app: :simplechat
+end
---
+++ test/simplechat_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule SimplechatWeb.PageControllerTest do
+  use SimplechatWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
