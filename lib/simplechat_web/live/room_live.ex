ee6149bab10a825f3089ce63d0ac65580aded29b
Car85 <car85@car85.car85>
2024-07-16 08:21:37 -0700

    adding phx-update in the div of the message

--- lib/simplechat_web/live/room_live.ex
+++ lib/simplechat_web/live/room_live.ex
@@ -6,25 +6,23 @@ defmodule SimplechatWeb.RoomLive do
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
     SimplechatWeb.Endpoint.subscribe(topic)
-    {:ok, assign(socket,  room_id: room_id,  topic: topic, messages: ["Welcome to SimpleChat"])}
+    {:ok,
+      assign(socket,
+        room_id: room_id,
+        topic: topic,
+        messages: ["Welcome to SimpleChat"], 
+        temporary_assigns: [messages: [] ])}
   end
 
   @impl true
    def handle_event("submit_message", %{"message" => message}, socket) do
-     topic = socket.assigns.topic
-     formatted_message = "#{topic}: #{message}"
-     messages = ["#{socket.assigns.room_id}: #{message}" | socket.assigns.messages]
      Logger.info(message: message) 
-     SimplechatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message",formatted_message)
+     SimplechatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message",message)
     {:noreply, socket}
    end
 
   @impl true
   def handle_info(%{event: "new-message", payload: message}, socket) do
-    topic = socket.assigns.topic
-    formatted_message = "#{topic}: #{message}"
-    messages = [formatted_message | socket.assigns.messages]
-    Logger.info(payload: message)
-    {:noreply, assign(socket, messages: socket.assigns.message ++ [message])}
+        {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
   end
 end
