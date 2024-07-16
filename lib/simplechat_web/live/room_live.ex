610a7e054635e45f25a3b1a07902dec98bed06c8
Car85 <car85@car85.car85>
2024-07-16 05:29:33 -0700

    probas amb socket

--- lib/simplechat_web/live/room_live.ex
+++ lib/simplechat_web/live/room_live.ex
@@ -6,13 +6,25 @@ defmodule SimplechatWeb.RoomLive do
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
     SimplechatWeb.Endpoint.subscribe(topic)
-    {:ok, assign(socket, messages: [], room_id: room_id, message: "", topic: topic, messages: ["Welcome to SimpleChat"])}
+    {:ok, assign(socket,  room_id: room_id,  topic: topic, messages: ["Welcome to SimpleChat"])}
   end
 
   @impl true
    def handle_event("submit_message", %{"message" => message}, socket) do
-    messages = [{socket.assigns.room_id, message} | socket.assigns.messages]
-    Logger.info(message: message) 
-    {:noreply, assign(socket, messages: messages, message: "")}
+     topic = socket.assigns.topic
+     formatted_message = "#{topic}: #{message}"
+     messages = ["#{socket.assigns.room_id}: #{message}" | socket.assigns.messages]
+     Logger.info(message: message) 
+     SimplechatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message",formatted_message)
+    {:noreply, socket}
+   end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    topic = socket.assigns.topic
+    formatted_message = "#{topic}: #{message}"
+    messages = [formatted_message | socket.assigns.messages]
+    Logger.info(payload: message)
+    {:noreply, assign(socket, messages: socket.assigns.message ++ [message])}
   end
 end
