ea319bdac8368cf1c5d03ca238cc24ba3a29d541
Car85 <car85@car85.car85>
2024-07-17 02:32:06 -0700

    users-list

--- lib/simplechat_web/live/room_live.ex
+++ lib/simplechat_web/live/room_live.ex
@@ -5,26 +5,27 @@ defmodule SimplechatWeb.RoomLive do
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
+    username = MnemonicSlugs.generate_slug(2)
     if connected?(socket), do: SimplechatWeb.Endpoint.subscribe(topic)
     {:ok,
       assign(socket,
         room_id: room_id,
         topic: topic,
         new_message: "",
-        messages: [%{uuid: UUID.uuid4(), content: "Welcome to SimpleChat"}], 
+        username: username,
+        messages: [%{uuid: UUID.uuid4(), content: "Welcome to SimpleChat #{username}", username: "system"}], 
         temporary_assigns: [messages: [] ])}
   end
 
   @impl true
    def handle_event("submit_message", %{"message" => message}, socket) do
-     message = %{uuid: UUID.uuid4(), content: message}
+     message = %{uuid: UUID.uuid4(), content: message, username: socket.assigns.username}
      SimplechatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message",message)
     {:noreply, assign(socket, new_message: "" )}
    end
 
   @impl true
    def handle_event("form_update", %{"message" => message}, socket) do
-     Logger.info("Form updated with message: #{message}")
      {:noreply, assign(socket, new_message: message)}
    end
 
